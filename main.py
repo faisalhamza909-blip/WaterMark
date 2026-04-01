eacbda74d5c866c632250d7035070c1e5869ed4a
Hamza <faisalhamza909@gmail.com>
2026-04-01 08:18:17 +0200

    added text watermark func

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from PIL import Image, ImageFont, ImageDraw
-img = Image.open("random_img.jpg").convert("RGBA")
-draw = ImageDraw.Draw(img)
-font = ImageFont.load_default()
-draw.text((50,50),"Hamza",fill=(255,255,255,128),font= font)
-img.show()
+
+
+
+def watermark_text(input_path, text):
+    img = Image.open(input_path).convert("RGBA")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.load_default()
+
+    width, height = img.size
+
+    bbox = draw.textbbox((0, 0), text, font=font)
+    text_width = bbox[2] - bbox[0]
+    text_height = bbox[3] - bbox[1]
+
+    x = width - text_width - 10
+    y = height - text_height - 10
+
+    draw.text((x, y), text, fill=(255, 255, 255, 128), font=font)
+
+    img.save("output.png")
+    img.show()
+
+
+text = input("Enter the text\n")
+watermark_text("random_img.jpg", text)
